845c6a93e21e894fe240198c62e6cad13fa47158
grizet_j <jean.grizet@epitech.eu>
2016-10-07 11:26:44 +0100

    Eae fix uuid gen

--- notebook/static/notebook/js/notebook.js
+++ notebook/static/notebook/js/notebook.js
@@ -390,7 +390,7 @@ import {ShortcutEditor} from 'notebook/js/shortcuteditor';
 			var uuid = 'xxxxxxxx'.replace(/[x]/g, function(c) {
 				var r = (d + Math.random()*16)%16 | 0;
 				d = Math.floor(d/16);
-				return (r&0x3|0x8)).toString(16);
+				return r.toString(16);
 			});
 			return uuid;
 		};
