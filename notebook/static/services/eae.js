33931679454505123c097de1c9dcfe128b592a80
grizet_j <jean.grizet@epitech.eu>
2016-10-12 15:28:06 +0100

    Eae class constructor fix typo ulr -> url

--- notebook/static/services/eae.js
+++ notebook/static/services/eae.js
@@ -6,9 +6,7 @@ function(utils) {
 
     var Eae = function(options) {
 		this.eae_url = "146.169.15.140:8080";
-		this.base_rul = options.base_url;
-		console.log(options);
-		console.log(this.base_url);
+		this.base_url = options.base_url;
     };
 	
 	Eae.prototype.api_eae = function() {
