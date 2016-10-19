e9a877d170e4b915a7bc1329628bfe9e8cc14b92
grizet_j <jean.grizet@epitech.eu>
2016-10-19 14:26:54 +0100

    Eae managment query cluster list

--- notebook/static/notebook/js/notebook.js
+++ notebook/static/notebook/js/notebook.js
@@ -461,7 +461,9 @@ import {ShortcutEditor} from 'notebook/js/shortcuteditor';
 				console.log(list_ok);
 				var input = $(cluster_field).find("[name='eae-submit-cluster']");
 				console.log(input);
+				console.log("Got to iter");
 				list_ok.forEach(function(item, idx) {
+					console.log("Inner loop");
 					console.log(item);
 					var entry = $("<option name='eae-submit-cluster' value='" + item['name'] + 
 								  ">[" + item['type'] + "] - " + item['name'] + "</option>");
