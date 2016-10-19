d5a02113cc71240f0662bbd4b566ebfff175f41b
grizet_j <jean.grizet@epitech.eu>
2016-10-19 12:36:25 +0100

    Eae managment query cluster list

--- notebook/static/services/eae.js
+++ notebook/static/services/eae.js
@@ -39,7 +39,7 @@ function(utils) {
 		var settings = {
             type : "GET"
         };
-        var url = this.api_eae("utilities/isAlive");
+        var url = this.api_eae("eAEManagement/retrieveClusters");
 		console.log(url);
 		
 		return utils.promising_ajax(url, settings);
