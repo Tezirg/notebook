c0f9b34d55202b53414c9a5a165363081a3a1778
grizet_j <jean.grizet@epitech.eu>
2016-10-20 15:26:51 +0100

    Debug step 3 css

--- notebook/static/notebook/js/notebook.js
+++ notebook/static/notebook/js/notebook.js
@@ -482,7 +482,7 @@ import {ShortcutEditor} from 'notebook/js/shortcuteditor';
 					if (idx == 0)
 						elem.addClass("chosen");
 					elem.click(function(event) {
-						cluster_list.find('li').removeClass("chosen");
+						cluster_list.find("li").removeClass("chosen");
 						 $( event.delegateTarget ).addClass("chosen");
 					})
 				});
