0a0f7908ae8591c2b56095d94bdd9d1f394b39f5
grizet_j <jean.grizet@epitech.eu>
2016-10-24 11:11:29 +0100

    Step2 remove input fields

--- notebook/static/notebook/js/notebook.js
+++ notebook/static/notebook/js/notebook.js
@@ -478,7 +478,10 @@ import {ShortcutEditor} from 'notebook/js/shortcuteditor';
 		var step_2_params = step_2_body.find('#param-list');
 		
 		step_2_button.click(function(event) {
-			var new_param = $('<div><input type="text" value=""></div>');
+			var new_param = $('<div><input type="text" value=""><button type="button" class="btn btn-danger">X</button></div>');
+			new_param.find('button').click(function(event) {
+				step_2_params.remove(new_param);
+			})
 			step_2_params.append(new_param);
 		});
 		
