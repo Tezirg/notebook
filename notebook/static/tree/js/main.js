b421cc59dc5b15c0e27a3c3c9e2cc2b12d720848
Axel Oehmichen <axelfrancois.oehmichen11@imperial.ac.uk>
2017-04-15 16:58:21 +0200

    xxxxxxx

--- notebook/static/tree/js/main.js
+++ notebook/static/tree/js/main.js
@@ -201,7 +201,7 @@ require([
         $("#tabs").find("a[href=" + window.location.hash + "]").click();
     }
 
-    const myDropzone = new dropzone.Dropzone("div#mydropzone", {
+    const myDropzone = new Dropzone("div#mydropzone", {
         maxFilesize: 10000,// MB
         parallelUploads: 3,
         uploadMultiple: true,
