985162d006c027134c246d0480da10e427e77e1e
Axel Oehmichen <axelfrancois.oehmichen11@imperial.ac.uk>
2017-04-20 12:59:51 +0100

    cleaning up and almost working

--- notebook/services/uploadfiles/handlers.py
+++ notebook/services/uploadfiles/handlers.py
@@ -4,12 +4,11 @@ import logging
 import re
 
 from tornado import web, gen
-from tornado.web import RequestHandler, stream_request_body, HTTPError
+from tornado.web import stream_request_body, HTTPError
 
 from ...base.handlers import APIHandler, json_errors
 
 UPLOAD_PATH = "/home/eae/jupyter/"
-# UPLOAD_KEYS = {} # {'filename': UploadFile obj}
 
 class UploadFile:
 
@@ -21,14 +20,7 @@ class UploadFile:
         self.content_type = ''
         self.read_bytes = 0
         self.chunk_number = 0
-        self.file = open(self.filepath, 'wb')
-
-        # self.client_key = self.request.query_arguments.get('key', None)
-        # if not self.client_key:
-        #     raise 'No "key" (GET) parameter!'
-        # else:
-        #     self.client_key = self.client_key[0]
-        #     print(self.client_key)
+        self.file = open(self.filepath, 'ab')
 
         try:
             self.content_length = int(self.request.headers.get('Content-Length'))
@@ -38,26 +30,6 @@ class UploadFile:
     @gen.coroutine
     def percentages(self):
         return int(round(self.read_bytes / self.content_length, 2) * 100)
-
-
-# class UploadForm(RequestHandler):
-#
-#     @gen.coroutine
-#     def get(self):
-#         self.render("upload_form.html")
-#
-#
-# class UploadPercentages(RequestHandler):
-#
-#     @gen.coroutine
-#     def get(self):
-#         try:
-#             client_key = str(self.get_argument('key'))
-#             file = UPLOAD_KEYS.get(client_key, None)
-#             p = yield file.percentages() if file else ''
-#             self.write('{}%'.format(p))
-#         except Exception as e:
-#             raise('Exception "{0}" occurred while get file upload percentages!'.format(str(e)))
 
 
 @stream_request_body
@@ -70,25 +42,14 @@ class UploadFilesHandler(APIHandler):
             self.file = UploadFile(self.request)
         except Exception as e:
             raise HTTPError(500)
-        # UPLOAD_KEYS[self.file.client_key] = self.file
-
-    # @gen.coroutine
-    # def data_received(self, chunk):
-    #     print len(chunk)
-    #     print chunk
-    #     with open(UPLOAD_FOLDER + self.uuid, 'a') as f:
-    #         f.write(chunk)
-    #         f.close()
 
     @gen.coroutine
     def data_received(self, chunk):
-        print "data_received"
         self.file.read_bytes += len(chunk)
         self.file.chunk_number += 1
 
         if self.file.chunk_number == 2:
             try:
-                print "In the try catch"
                 chunk = yield self._get_head(chunk)
             except Exception as e:
                 logging.error('Exception "{0}" occurred while parse first chunk.\n'
@@ -106,7 +67,8 @@ class UploadFilesHandler(APIHandler):
                               .format(str(e), self.file.read_bytes))
                 raise HTTPError(500)
 
-        self.file.file.write(chunk)
+        if(len(chunk) > 1 ):
+            self.file.file.write(chunk)
 
         if self.file.read_bytes == self.file.content_length:
             self.file.chunk_number = 0
@@ -123,15 +85,12 @@ class UploadFilesHandler(APIHandler):
         http://www.w3.org/TR/html401/interact/forms.html#h-17.13.4.2
         https://docs.python.org/3/library/stdtypes.html#bytes.partition
         """
-        print "in the get_head"
         head_arr = chunk.partition(b'\r\n\r\n')
-        name_match = re.match('.* filename="([^"]*)"', str(head_arr[0]))
+        name_match = re.search('.* filename=\"(.*)\"', str(head_arr[0]))
         self.file.original_filename = name_match.group(1)
-        ct_match = re.match('.*\s*Content-Type: (.*)\'', str(head_arr[0]))
+        ct_match = re.search('.*\s*Content-Type: (.*)', str(head_arr[0]))
         self.file.content_type = ct_match.group(1)
         chunk = head_arr[2]  # replace with clean body
-        print chunk
-        # raise ValueError('Wrong filename!')
         return chunk
 
     @gen.coroutine
@@ -143,17 +102,8 @@ class UploadFilesHandler(APIHandler):
 
     @web.authenticated
     @json_errors
-    @gen.coroutine
     def put(self):
-        # filename = "toto.txt"
-        # mtype = self.request.headers.get('Content-Type')
-        # self.get_body_argument()
-        # print 'PUT "%s" "%s" %d bytes', filename, mtype, self.bytes_read
-        # os.rename(UPLOAD_FOLDER + self.uuid, filename)
-        print "In the PUT"
-        print('ok:')
-        # print(UPLOAD_KEYS)
-        # self.write('{}'.format(UPLOAD_KEYS))
+        print 'Uploaded: {}\n Length: {}\n Content Type: {}'.format(self.file.original_filename, self.file.content_length, self.file.content_type)
         return 200
 
 
